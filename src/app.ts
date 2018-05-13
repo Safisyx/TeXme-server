f33b74955a4202a0983ad38b963d0bdb065d0a08
Safisyx <safisyx@gmail.com>
2018-05-14 00:38:46 +0200

    Use login and user controllers

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import 'reflect-metadata'
 import {useKoaServer} from "routing-controllers"
 import * as Koa from 'koa'
 
+import UserController from './controllers/users'
+import LoginController from './controllers/logins'
+
 const app = new Koa()
 
 useKoaServer(app,{
   cors: true,
   controllers: [
-    //
+    UserController,
+    LoginController
   ]
 })
 
