be12a2d223a36853c60f485d0043e54b6eeb53cb
Safisyx <safisyx@gmail.com>
2018-05-14 02:41:13 +0200

    Setup an email template for signup

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const emailSender = 'info@texme.com'
+export const clientUrl = process.env.CLIENT_URL || 'http://localhost:3000'
---
+++ src/mail/templates.ts
@@ -0,0 +1,28 @@
+import * as sgMail from '@sendgrid/mail'
+import {SENDGRID_KEY} from "../mailApiKey";
+import {clientUrl, emailSender} from '../constants'
+
+sgMail.setApiKey(SENDGRID_KEY)
+
+export const sendSignUpMail = (email: string, token:string) => {
+  const baseUrl = `${clientUrl}/email_confirmation?token=`
+  const msg = {
+    to: email,
+    from: emailSender,
+    subject: 'Verify your email for TeXme',
+    text: `Hello,\n
+             \n
+             Thank you for your will to use TexMe!\n\n
+             Please go the the link below to confirm your email address
+             \n
+             ${baseUrl + token}\n\n
+             Cheers!`,
+    html: `<p>
+          Hello,<br/><br/>
+             Thank you for your will to use TexMe!<br/><br/>
+             Please go to the link below to confirm your email address<br/>
+          <a href="${baseUrl + token}">${baseUrl + token}</a><br/><br/>
+          Cheers!</p>`,
+  }
+  return sgMail.send(msg)
+}
