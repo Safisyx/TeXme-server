650f9a37e39254b9b2682d6e58054909f91af03b
Safisyx <safisyx@gmail.com>
2018-05-16 00:23:23 +0200

    Add middlewares for authorization and currentuser

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata'
-import {useKoaServer} from "routing-controllers"
+import {useKoaServer, Action, BadRequestError} from "routing-controllers"
 import * as Koa from 'koa'
-
+import {verify} from './jwt'
 import UserController from './controllers/users'
 import LoginController from './controllers/logins'
 
@@ -12,7 +12,32 @@ useKoaServer(app,{
   controllers: [
     UserController,
     LoginController
-  ]
+  ],
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization;
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ');
+
+      try {
+        return !!(token && verify(token));
+      } catch (e) {
+          throw new BadRequestError(e);
+        }
+    }
+    return false;
+  },
+  currentUserChecker: async (action: Action) => {
+    const header: string = action.request.headers.authorization;
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ');
+
+      if (token) {
+        const { id, role } = verify(token);
+        return { id, role }
+      }
+    }
+    return undefined;
+  }
 })
 
 export default app
