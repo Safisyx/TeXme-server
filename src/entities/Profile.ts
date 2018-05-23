490f86fec1e2abcdbb19b23558756cd7266fddb4
Safisyx <safisyx@gmail.com>
2018-05-23 21:43:38 +0200

    Use bigint for id in Profile

--- src/entities/Profile.ts
+++ src/entities/Profile.ts
@@ -1,10 +1,11 @@
-import {Entity, PrimaryGeneratedColumn, Column, OneToOne} from 'typeorm'
+import {Entity, PrimaryColumn, Column, Generated, OneToOne} from 'typeorm'
 import {BaseEntity} from 'typeorm/repository/BaseEntity'
 import {IsString, MinLength, IsDate} from 'class-validator'
 import {User} from './User'
 @Entity()
 export class Profile extends BaseEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryColumn('bigint')
+  @Generated()
   id?: number
 
   @IsString()
